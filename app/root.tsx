06e558be2c1671d9c629e066d8e0ef306393d7e5
Beau Lazear <beaulazear@gmail.com>
2025-11-07 09:56:57 -0500

    new favicon implementation

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,9 @@ import "./app.css";
 import { AuthProvider } from "./lib/auth-context";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", href: "/trainer-log/favicon.ico", sizes: "any" },
+  { rel: "icon", href: "/trainer-log/favicon.ico", type: "image/png" },
+  { rel: "apple-touch-icon", href: "/trainer-log/favicon.ico" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
